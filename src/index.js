e04cc56f02c7c58b098093bfbd5179db791c823b
ghostnetrn <54438080+ghostnetrn@users.noreply.github.com>
2021-11-27 20:09:41 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Bottleneck from "bottleneck";
 import { handleMessage, handleError, percent } from "./utils";
 //import config from "./config.js";
 import { Telegraf, Markup } from 'telegraf';
-import { dependencies } from "../package.json"
+import dependencies from "../package.json"
 
 //let { amount, initialSell, intervalMs, test, differencelogger } = config;
 
@@ -75,11 +75,12 @@ bot.hears('🔛 Test Mode', async (ctx) => {
 );
 
 bot.hears('☸ Configs', (ctx) => {
+  let versionAPI = JSON.parse(dependencies)
   ctx.replyWithMarkdown(`
 ⏱️ *Intervalo*: ${intervalMs}ms
 ℹ️ *Modo teste*: ${test ? 'ativado' : 'desativado'}
 ℹ️ *InitialSell*: ${initialSell ? 'ativado' : 'desativado'}
-ℹ️ *Versão API*: ${JSON.parse(dependencies)}
+ℹ️ *Versão API*: ${versionAPI}
 💵 *Valor em operação*: ${amount}
 *O bot utiliza 90% do saldo em BTC ou BRL 
     `, keyboard)
